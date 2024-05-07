75f96f7f00bc19a40b84cb1d8043d48d73e0950f
Urmish Solanki <131744795+urmishsolanki2225@users.noreply.github.com>
2024-05-07 11:47:47 +0530

    Update welcome.blade.php
    Added Bu Urmish Solanki

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,7 +113,7 @@
     }
   </style>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBvtt0lycFTForXSvAX60V1GUQvUv7lgOU"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=Add Your key"></script>
 </head>
 
 <body>
